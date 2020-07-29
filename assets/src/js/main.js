5505ab446806f698d2441438b62c1bd66a4d971e
Jelle <jelle.buurman@live.nl>
2020-07-29 10:11:37 +0200

    First push

---
+++ assets/src/js/main.js
@@ -0,0 +1,29 @@
+const $ = require('jquery');
+require('slick-carousel');
+
+$(document).ready(function () {
+    $('body').addClass('loaded');
+});
+
+$('.hamburger-menu').click(function () {
+    if ($(this).hasClass("open")) {
+        $(this).removeClass("open");
+        $('aside').removeClass("opened");
+        $('html').css("overflow", "initial");
+    } else {
+        $(this).addClass("open");
+        $('aside').addClass("opened");
+        $('html').css("overflow", "hidden");
+
+    }
+});
+
+$('.more-info').click(function () {
+    if($(this).parent().hasClass('active')) {
+        $(this).html('Meer info')
+        $(this).parent().removeClass('active')
+    } else {
+        $(this).html('Minder info')
+        $(this).parent().addClass('active')
+    }
+});
